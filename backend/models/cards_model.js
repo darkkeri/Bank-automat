8d4f7c8b1e1f80d569864449c8bd3416958717cd
ArttuIivari <89580030+ArttuIivari@users.noreply.github.com>
2024-03-28 16:03:49 +0200

    cards pin bcrypt valmis

--- backend/models/cards_model.js
+++ backend/models/cards_model.js
@@ -1,17 +1,21 @@
 const db = require('../database');
-//const bcryptjs=req('bcryptjs');
+const bcryptjs=require('bcryptjs');
 
 const cards = {
     getAllCards(callback){
         return db.query("SELECT * FROM cards", callback);
     },
     addCards(newCards, callback){
-        return db.query("INSERT INTO cards VALUES(?,?,?,?,?,?,?)",
-        [newCards.idCards,newCards.cardnumber,newCards.pincode,newCards.type,newCards.tries,newCards.active,newCards.creditlimit], callback);
+        bcryptjs.hash(newCards.pincode,10,function(err,hashedpin){
+            return db.query("INSERT INTO cards VALUES(?,?,?,?,?,?,?)",
+            [newCards.idCards,newCards.cardnumber,hashedpin,newCards.type,newCards.tries,newCards.active,newCards.creditlimit], callback);
+        });
         },
     updateCards(idCards, updateData, callback){
+        bcryptjs.hash(updateData.pincode,10,function(err,hashedpin){
         return db.query("UPDATE cards SET cardnumber=?, pincode=?, type=?, tries=?, active=?, creditlimit=? WHERE idCards=?",
-        [updateData.cardnumber, updateData.pincode, updateData.type, updateData.tries, updateData.active, updateData.creditlimit, idCards],callback);
+        [updateData.cardnumber, hashedpin, updateData.type, updateData.tries, updateData.active, updateData.creditlimit, idCards],callback);
+        });
     },
     deleteCards(idCards, callback){
         return db.query("DELETE FROM cards WHERE idCards=?",[idCards], callback);
